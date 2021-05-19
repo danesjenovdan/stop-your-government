91c22fea1e259ed3d619cb5163dcd6ecc85aa902
LepkoQQ <lepko.san@gmail.com>
2021-05-19 18:58:53 +0200

    typing indicator style, improve message styles

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -3,7 +3,32 @@ import { MESSAGE_DISPLAY, useMessageDisplay } from '../utils/chat-hooks.js';
 import { useScrollToBottom } from '../utils/scroll-to-bottom.js';
 import { ChatResponse } from './ChatResponse.jsx';
 import { ImageWithPreview } from './ImageWithPreview.jsx';
+import { TypingIndicator } from './TypingIndicator.jsx';
 import styles from './ChatMessage.module.scss';
+
+const Avatar = ({ actor }) => {
+  if (!actor?.avatar?.url) {
+    return null;
+  }
+  return (
+    <div
+      className={styles.avatar}
+      style={{ backgroundImage: `url("${actor.avatar.url}")` }}
+    />
+  );
+};
+
+const Content = ({ actor, text, image }) => {
+  return (
+    <div className={styles.content}>
+      {!!actor?.name && <div className={styles.name}>{actor.name}</div>}
+      <div className={styles.bubble}>
+        {!!image && <ImageWithPreview image={image} className={styles.image} />}
+        {!!text && <div className={styles.text}>{text}</div>}
+      </div>
+    </div>
+  );
+};
 
 export const ChatMessage = ({ message, actor, pump }) => {
   const displayState = useMessageDisplay(message);
@@ -23,38 +48,16 @@ export const ChatMessage = ({ message, actor, pump }) => {
     return null;
   }
 
-  if (displayState === MESSAGE_DISPLAY.WRITING) {
-    return <div>...</div>; // TODO: style writing dots/bubbles
-  }
-
-  const avatar = actor?.avatar?.url ? (
-    <div
-      className={styles.avatar}
-      style={{ backgroundImage: `url("${actor.avatar.url}")` }}
-    />
-  ) : null;
-  const name = actor?.name ? (
-    <div className={styles.name}>{actor.name}</div>
-  ) : null;
-
   let content;
-  if (message.type === 'TEXT') {
+  if (displayState === MESSAGE_DISPLAY.WRITING) {
+    content = <Content actor={actor} text={<TypingIndicator />} />;
+  } else if (message.type === 'TEXT') {
     content = message.text?.length ? (
-      <div className={styles.content}>
-        {name}
-        <div className={styles.bubble}>
-          <div className={styles.text}>{message.text}</div>
-        </div>
-      </div>
+      <Content actor={actor} text={message.text} />
     ) : null;
   } else if (message.type === 'IMAGE') {
     content = message.file?.url ? (
-      <div className={styles.content}>
-        {name}
-        <div className={styles.bubble}>
-          <ImageWithPreview image={message.file} className={styles.image} />
-        </div>
-      </div>
+      <Content actor={actor} image={message.file} />
     ) : null;
   } else if (message.type === 'ACTION_MAIN_THREAD_BACK') {
     content = null;
@@ -66,7 +69,7 @@ export const ChatMessage = ({ message, actor, pump }) => {
     <>
       {!!content && (
         <div className={styles.message}>
-          {avatar}
+          <Avatar actor={actor} />
           {content}
         </div>
       )}
---
+++ src/components/TypingIndicator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './TypingIndicator.module.scss';
+
+export const TypingIndicator = () => {
+  return (
+    <div className={styles.typing}>
+      <span />
+      <span />
+      <span />
+    </div>
+  );
+};
