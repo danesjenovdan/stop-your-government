cec40e56579638a0d1dfe8569d7d07ab406982f8
LepkoQQ <lepko.san@gmail.com>
2022-01-13 21:02:19 +0100

    disable story poster at start of chat container

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StoryPoster } from './StoryPoster.jsx';
+// import { StoryPoster } from './StoryPoster.jsx';
 import { ChatMessages } from './ChatMessages.jsx';
 
 export const ChatContainer = ({ story, chat }) => (
   <>
-    <StoryPoster poster={chat.poster} />
+    {/* <StoryPoster poster={chat.poster} /> */}
     <ChatMessages story={story} chat={chat} />
   </>
 );
