4928b305c7e8c779665dc57c25be01c1b379d807
LepkoQQ <lepko.san@gmail.com>
2022-08-09 16:11:36 +0200

    lock chapters untill activation trigger

--- src/components/TriggerChat.jsx
+++ src/components/TriggerChat.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getChatLink } from '../utils/links.js';
+import {
+  getUnlockedChapters,
+  setUnlockedChapters,
+} from '../utils/variables.js';
 import { Button } from './Button.jsx';
 import { ImageWithPreview } from './ImageWithPreview.jsx';
 import styles from './TriggerChat.module.scss';
 
 export const TriggerChat = ({ story, chat }) => {
   const history = useHistory();
+
+  useEffect(() => {
+    const chapters = getUnlockedChapters();
+    chapters[story._id] = chapters[story._id] || [];
+    if (!chapters[story._id].includes(chat._id)) {
+      chapters[story._id].push(chat._id);
+      setUnlockedChapters(chapters);
+    }
+  }, []);
 
   const onClick = () => {
     return history.push(getChatLink(story._id, chat._id));
